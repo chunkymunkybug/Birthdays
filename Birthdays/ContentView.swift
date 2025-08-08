b39844a8f3b9dcd63a88c4508de661a6f3c02252
Maddy <m.s.woo2008@gmail.com>
2025-08-08 10:48:32 -0400

    created app

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var friends: [Friend] = [
+        Friend (name:"Maddy", birthday: .now),
+        Friend (name:"Sally", birthday: .now)
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack {//beginning of navStack
+
+        List(friends, id: \.name) {friend in
+            HStack {
+                Text(friend.name)
+                Spacer ()
+                Text (friend.birthday, format:
+                    .dateTime.month (.wide).day()
+                    .year ())
+                } //end of HStack
+            } //end of list
+            
+        .navigationTitle("Birthdays")
+        .safeAreaInset(edge: .bottom){
+            VStack(alignment: .center, spacing: 20){
+                Text ("New Birthday")
+                    .font(.headline)
+                DatePicker(selection:
+                            $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                    TextField("Name", text:$newName)
+                        .textFieldStyle(.roundedBorder)
+                }//end of DatePicker
+                Button ("Save"){
+                    let newFriend =
+                        Friend(name: newName, birthday: newBirthday)
+                    friends.append (newFriend)
+                    newName = ""
+                    newBirthday = .now
+                }
+                
+                }//end of VStack
+            .padding ()
+            .background(.bar)
+
+            }//end of safeAreaInset
+        }//end of NavStack
+    }//end of var body: some View
+} //end of struct content view
 
 #Preview {
     ContentView()
 }
+
+
